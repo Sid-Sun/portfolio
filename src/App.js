23f4f41d6ce9e6c2f824a50db1a7da74e16eec2e
Sid Sun <sid@sidsun.com>
2019-09-23 16:07:20 +0530

    Add CSS, relevant favicons
    Signed-off-by: Sid Sun <sid@sidsun.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/brands.min.css'
+import '@fortawesome/fontawesome-free/css/solid.min.css'
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import {BrowserRouter} from "react-router-dom";
 import Root from './components/Root'
 
