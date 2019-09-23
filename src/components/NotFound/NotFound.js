5e8ee6c08a4ac161c3bef649f2cbc45d75ea4d82
Sid Sun <sid@sidsun.com>
2019-09-23 16:43:23 +0530

    Error 404, this commit message was not found
    Signed-off-by: Sid Sun <sid@sidsun.com>

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+    return (
+        <div className="container text-center vertical-center-nf">
+            <h1 className="text-center">
+              Like a lot of things in life, this page could not, unfortunately; be found :(
+            </h1>
+        </div>
+    );
+}
+export default NotFound;
