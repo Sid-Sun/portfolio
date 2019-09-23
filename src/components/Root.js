3a35f94167091276936c7f77bbdde53bfc2cbc07
Sid Sun <sid@sidsun.com>
2019-09-23 16:40:24 +0530

    Update root component
    Signed-off-by: Sid Sun <sid@sidsun.com>

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,23 +1,36 @@
-import React, { Component } from 'react';
+import React, {PureComponent} from 'react';
 import NavigationBar from "./NavigationBar/NavigationBar";
 import {Route, Switch} from "react-router-dom";
+import Home from './Home/Home'
+import Skills from './Skills/Skills'
+import Projects from './Projects/Projects'
+// import ProjectDetails from "./Projects/ProjectDetails/ProjectDetails";
+import Footer from "./Footer/Footer";
+import NotFound from "./NotFound/NotFound";
 
-
-export default class Root extends Component {
-  render() {
-    return (
-      <div className="root">
-        <NavigationBar/>
-        <div className="w-100" style={{
-            height: '61px'
-        }}/> {/*Shift all content down by 61px to account for navbar height*/}
-        <Switch>
-          <Route path="/" exact render={() => <h1>HOME</h1>}/>
-          <Route path="/skills" render={() => <h1>Skills</h1>}/>
-          <Route path="/projects" render={() => <h1>Projects</h1>}/>
-          <Route path="/socials" render={() => <h1>Socials</h1>}/>
-        </Switch>
-      </div>
-    )
-  }
+export default class Root extends PureComponent {
+    render() {
+        return (
+            <div className="root">
+                <NavigationBar/>
+                <div className="w-100" style={{
+                    height: '61px'
+                }}/>                {/*Shift all content down by 61px to account for navbar height*/}
+                <div id="content">
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/skills" component={Skills}/>
+                        {/*<Route path="/projects/:project" component={ProjectDetails}/>*/}
+                        <Route path="/projects" component={Projects}/>
+                        <Route path="/" component={NotFound}/>
+                        {/*<Route path="/socials" render={() => <h1>Socials</h1>}/>*/}
+                    </Switch>
+                    <Footer />
+                    <div className="w-100" style={{
+                        height: '56px'
+                    }} id="bottom-nav-adj"/>
+                </div>
+            </div>
+        )
+    }
 }
